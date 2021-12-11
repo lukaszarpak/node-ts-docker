864e310b0bb1869f29bfce958a940c7edc368247
lukaszarpak <szarpak90@gmail.com>
2021-12-11 21:17:07 +0100

    add support for environmental variables

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import express from "express";
 import type { Application } from "express";
 import defaultController from "./controllers/defaultController";
 
+// Env config
+import dotenv from "dotenv";
+const envPath = process.env?.NODE_ENV || "dev";
+dotenv.config({ path: `.env.${envPath}` });
+
+console.log("test", process.env.test);
+
 const app: Application = express();
 const port = 3000;
 
