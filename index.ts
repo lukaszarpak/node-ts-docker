4d4f837f90cb7742d8457573a9c00f99fade4602
lukaszarpak <szarpak90@gmail.com>
2021-12-11 18:08:38 +0100

    make it work

--- index.ts
+++ index.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import type { Application } from "express";
+import defaultController from "./controllers/defaultController";
 
 const app: Application = express();
-const port = "";
+const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
+app.get("/", defaultController);
 
 app.listen(port, (): void => {
   console.log(`App is listeing on port: http://localhost:${port}`);
